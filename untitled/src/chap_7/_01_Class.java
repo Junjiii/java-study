b935b7d4aab64dbbe6471006cd173da1a3d70373
Jjunjiii’ <drum1701@gmail.com>
2024-06-09 11:27:49 +0900

    _01_Class

---
+++ untitled/src/chap_7/_01_Class.java
@@ -0,0 +1,19 @@
+package chap_7;
+
+public class _01_Class {
+    public static void main(String[] args) {
+        // 객체 지향 프로그래밍 (OOP : Objected-Oriented Programming)
+        // 블랙박스
+
+        String modelName = "까망이";
+        String resolution = "FHD";
+        int price = 200000;
+        String color  = "Black";
+
+        System.out.println(modelName);
+        System.out.println(resolution);
+        System.out.println(price);
+        System.out.println(color);
+
+    }
+}
