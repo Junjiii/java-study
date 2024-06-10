f94227e8389b1c778fa6ccacd6a4a0e05b3a8d6c
Jjunjiii’ <drum1701@gmail.com>
2024-06-10 21:09:40 +0900

    _02_InstanceVariables

--- untitled/src/chap_7/_01_Class.java
+++ untitled/src/chap_7/_01_Class.java
@@ -4,16 +4,20 @@ public class _01_Class {
     public static void main(String[] args) {
         // 객체 지향 프로그래밍 (OOP : Objected-Oriented Programming)
         // 블랙박스
+        Blackbox b1 = new Blackbox();
+        // Blackbox 클래스로부터 b1 객체 생성
+        // b1 객체는 blackbox 클래스의 인스턴스
 
-        String modelName = "까망이";
-        String resolution = "FHD";
-        int price = 200000;
-        String color  = "Black";
+        b1.modelName = "까망이";
+        b1.resolution = "FHD";
+        b1.price = 200000;
+        b1.color = "Black";
 
-        System.out.println(modelName);
-        System.out.println(resolution);
-        System.out.println(price);
-        System.out.println(color);
+
+        System.out.println(b1.modelName);
+        System.out.println(b1.resolution);
+        System.out.println(b1.price);
+        System.out.println(b1.color);
 
     }
 }
