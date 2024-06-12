10f785fff7cbb976c58d55698f83a6dd6f3d7b28
Jjunjiii’ <drum1701@gmail.com>
2024-06-12 20:07:02 +0900

    _09_Getter&Setter

--- untitled/src/chap_7/_07_This.java
+++ untitled/src/chap_7/_07_This.java
@@ -4,7 +4,8 @@ public class _07_This {
     public static void main(String[] args) {
         Blackbox b1 = new Blackbox();
         b1.modelName = "까망이";
-        b1.appendModelName("(최신형)");
+        String newName = "(최신형)";
+        b1.appendModelName(newName);
         System.out.println(b1.modelName);
     }
 }
