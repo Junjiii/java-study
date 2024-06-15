0cb1687c2ef352682abd15ceeb4e95551b65dce6
Jjunjiii’ <drum1701@gmail.com>
2024-06-15 21:33:23 +0900

    _02_GenericsClass

---
+++ untitled/src/chap_09/coffee/CoffeeByName.java
@@ -0,0 +1,15 @@
+package chap_09.coffee;
+
+import javax.naming.event.ObjectChangeListener;
+
+public class CoffeeByName {
+    public Object name;
+
+    public CoffeeByName(Object name) {
+        this.name = name;
+    }
+
+    public void ready() {
+        System.out.println("커피 준비 완료 : " + name);
+    }
+}
