79760c3616fe6382bc26e18107928f70f5b14796
Jjunjiii’ <drum1701@gmail.com>
2024-06-15 18:11:07 +0900

    _02_Interface

--- untitled/src/chap_08/camera/FactoryCam.java
+++ untitled/src/chap_08/camera/FactoryCam.java
@@ -1,8 +1,35 @@
 package chap_08.camera;
 
-public class FactoryCam extends Camera {
+import chap_08.detector.Detectorable;
+import chap_08.reporter.Reportable;
+
+public class FactoryCam extends Camera implements Detectorable, Reportable {
+    private Detectorable detector;
+    private Reportable reporter;
+
+    public void setDetector(Detectorable detector) {
+        this.detector = detector;
+    }
+
+    public void setReporter(Reportable reporter) {
+        this.reporter = reporter;
+    }
+
+
     @Override
     public void showMainFeature() {
         System.out.println("화재 감지");
     }
+
+
+    @Override
+    public void detect() {
+        detector.detect();
+    }
+
+    @Override
+    public void report() {
+        reporter.report();
+    }
 }
+
